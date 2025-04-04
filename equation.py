ca5f36ea710b2e19c7f1e204b9ca84837898534e
invia <inviakonstantin@gmail.com>
2025-04-05 01:42:07 +0300

    Revert "Помилковий коміт", коміт був зроблений випадково
    This reverts commit 6a08a46bde3b4fd0bdc9175d08c31caa5bf484ea.

--- equation.py
+++ equation.py
@@ -5,7 +5,7 @@ import os
 def solve_quadratic(a, b, c):
     if a == 0:
         raise ValueError("Error. a cannot be 0")
-    discriminant = b ** 2 + 4 * a * c
+    discriminant = b ** 2 - 4 * a * c
     if discriminant > 0:
         x1 = (-b + math.sqrt(discriminant)) / (2 * a)
         x2 = (-b - math.sqrt(discriminant)) / (2 * a)
