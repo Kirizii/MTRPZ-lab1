013a4675c3fb5e862413c61e48b7a4f3449c7390
invia <inviakonstantin@gmail.com>
2025-04-04 19:39:06 +0300

    Додано підтримку файлів у неінтерактивному режимі, додано функцію виводу кількості коренів рівняння

--- equation.py
+++ equation.py
@@ -28,12 +28,7 @@ def get_valid_float(prompt, non_zero=False):
             print("Error. Expected a valid real number.")
 
 
-def interactive_mode():
-    a = get_valid_float("a = ", non_zero=True)
-    b = get_valid_float("b = ")
-    c = get_valid_float("c = ")
-    print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
-    roots = solve_quadratic(a, b, c)
+def roots_number(roots):
     if len(roots) == 2:
         print(f"There are 2 roots\nx1 = {roots[0]}\nx2 = {roots[1]}")
     elif len(roots) == 1:
@@ -41,10 +36,35 @@ def interactive_mode():
     else:
         print("There are 0 roots")
 
+def interactive_mode():
+    a = get_valid_float("a = ", non_zero=True)
+    b = get_valid_float("b = ")
+    c = get_valid_float("c = ")
+    print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+    roots = solve_quadratic(a, b, c)
+    roots_number(roots)
+
+
+def noninteractive_mode(file_path):
+    if not os.path.exists(file_path):
+        print(f"File {file_path} does not exist")
+        sys.exit(1)
+    try:
+        with open(file_path, "r") as f:
+            line = f.readline().strip()
+            a, b, c = map(float, line.split())
+            print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+            roots = solve_quadratic(a, b, c)
+            roots_number(roots)
+    except ValueError:
+        print("invalid file format")
+        sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         interactive_mode()
+    elif len(sys.argv) == 2:
+        noninteractive_mode(sys.argv[1])
     else:
         print("Usage: python equation.py [filename]")
         sys.exit(1)
