31d7d8a21831490bd9722981aff399b1fe50beeb
invia <inviakonstantin@gmail.com>
2025-04-04 19:25:42 +0300

    Додано інтерактивний режим введення коефіцієнтів

--- equation.py
+++ equation.py
@@ -1,10 +1,10 @@
 import math
-
+import sys
+import os
 
 def solve_quadratic(a, b, c):
     if a == 0:
         raise ValueError("Error. a cannot be 0")
-
     discriminant = b ** 2 - 4 * a * c
     if discriminant > 0:
         x1 = (-b + math.sqrt(discriminant)) / (2 * a)
@@ -15,3 +15,36 @@ def solve_quadratic(a, b, c):
         return (x1,)
     else:
         return ()
+
+
+def get_valid_float(prompt, non_zero=False):
+    while True:
+        try:
+            value = float(input(prompt))
+            if non_zero and value == 0:
+                raise ValueError
+            return value
+        except ValueError:
+            print("Error. Expected a valid real number.")
+
+
+def interactive_mode():
+    a = get_valid_float("a = ", non_zero=True)
+    b = get_valid_float("b = ")
+    c = get_valid_float("c = ")
+    print(f"Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0")
+    roots = solve_quadratic(a, b, c)
+    if len(roots) == 2:
+        print(f"There are 2 roots\nx1 = {roots[0]}\nx2 = {roots[1]}")
+    elif len(roots) == 1:
+        print(f"There are 1 roots\nx1 = {roots[0]}")
+    else:
+        print("There are 0 roots")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        interactive_mode()
+    else:
+        print("Usage: python equation.py [filename]")
+        sys.exit(1)
